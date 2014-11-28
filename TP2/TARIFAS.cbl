175aed505d7ffa31b84d673e8feb11dcba4f8f05
Mat <matubat91@gmail.com>
2014-11-28 19:04:20 -0300

    Create TARIFAS.cbl

---
+++ TP2/TARIFAS.cbl
@@ -0,0 +1,39 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       PROGRAM-ID. TARIFAS.
+       ENVIRONMENT DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       CONFIGURATION SECTION.
+      *-----------------------
+       INPUT-OUTPUT SECTION.
+      *-----------------------
+       DATA DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       FILE SECTION.
+      *-----------------------
+       WORKING-STORAGE SECTION.
+      *-----------------------
+       LINKAGE SECTION.
+      **-*-*-*-*-*-*-*-*-*-*-*-*-*
+       01 PAR-ENTRADA.
+           03 PRIMER-OPERANDO PIC 9(3).
+       01 PAR-SALIDA.
+           03 RESULTADO PIC 9(5).
+       01 COD-OPER PIC X.
+       PROCEDURE DIVISION USING PAR-ENTRADA,PAR-SALIDA,COD-OPER.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       MAIN-PROCEDURE.
+      **
+      * The main procedure of the program
+      **
+        DISPLAY "Hello world"
+        MOVE 0 TO PAR-SALIDA
+        STOP RUN.
+      ** add other procedures here
+       END PROGRAM TARIFAS. 
